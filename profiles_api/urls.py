6a36ade3dcc2404ac98f5b79090bb6177c73ffa1
Suzanne <suzanne.baltar@gmail.com>
2021-03-10 11:21:04 -1000

    added profiles viewset with permissions

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,6 +6,8 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet) #queryset in ViewSet replaces base_name
+
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
